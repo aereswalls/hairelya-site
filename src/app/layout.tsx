573aab09bc444ccab30478d2f787467420c2072f
MyPointViews <leonardomazzone94@gmail.com>
2025-06-06 17:33:56 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { CssBaseline, ThemeProvider } from '@mui/material'
-import theme from '@/lib/theme'
+import theme from '../lib/theme' // ✅ percorsorelativo che funziona sempre
 
 export const metadata = {
   title: 'Hairelya – Your hair care ritual',
